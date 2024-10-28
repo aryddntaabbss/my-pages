cacc274225e63caffb1c25890832247c7181ac56
aryaddinata <aryaddinataabbas017@gmail.com>
2024-10-28 19:13:06 +0900

    Third Commit

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-
-function encryptText() {
-    const inputText = document.getElementById('inputText').value;
-    const shift = parseInt(document.getElementById('shift').value);
-    document.getElementById('outputText').value = caesarCipher(inputText, shift);
+function encryptText ()
+{
+    const inputText = document.getElementById( 'inputText' ).value;
+    const shift = parseInt( document.getElementById( 'shift' ).value );
+    document.getElementById( 'outputText' ).value = caesarCipher( inputText, shift );
 }
 
-function decryptText() {
-    const inputText = document.getElementById('inputText').value;
-    const shift = parseInt(document.getElementById('shift').value);
-    document.getElementById('outputText').value = caesarCipher(inputText, -shift);
+function decryptText ()
+{
+    const inputText = document.getElementById( 'inputText' ).value;
+    const shift = parseInt( document.getElementById( 'shift' ).value );
+    document.getElementById( 'outputText' ).value = caesarCipher( inputText, -shift );
 }
 
-function caesarCipher(text, shift) {
+function caesarCipher ( text, shift )
+{
     return text
-        .split('')
-        .map(char => {
-            if (char.match(/[a-z]/i)) {
-                const code = char.charCodeAt(0);
+        .split( '' )
+        .map( char =>
+        {
+            if ( char.match( /[a-z]/i ) )
+            {
+                const code = char.charCodeAt( 0 );
                 const base = code >= 65 && code <= 90 ? 65 : 97;
-                return String.fromCharCode(((code - base + shift) % 26) + base);
+                return String.fromCharCode( ( ( code - base + shift ) % 26 ) + base );
             }
             return char;
-        })
-        .join('');
+        } )
+        .join( '' );
 }
